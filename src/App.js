9faa3472d01190ad5f2839e5b0cb056b0168e9cf
cdsalty <soltiscd@gmail.com>
2019-11-13 16:02:29 -0500

    working Nav, Logo and ImageLinkForm Components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import "./App.css";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        <ImageLinkForm />
+      </div>
+    );
+  }
 }
 
 export default App;
+
+/*
+Starting components:
+  - Navigation      - COMPLETED
+  - Logo            - COMPLETED
+  - ImageLinkForm   
+  - FaceRecognition 
+*/
---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./ImageLinkForm.css";
+
+const ImageLinkForm = () => {
+  return (
+    <div>
+      <p class="f3">
+        {
+          "This little magic brain can detect faces in pictures. Give it a try..."
+        }
+      </p>
+      <div className="center">
+        <div className="form center pa4 br3 shadow-5">
+          <input className="f4 pa2 w-70 center" type="text" />
+          <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
+            Detect
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ImageLinkForm;
