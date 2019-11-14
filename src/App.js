725cb7a419dfb14201659d4a83490a6674d6167b
cdsalty <soltiscd@gmail.com>
2019-11-13 23:02:29 -0500

    finsihed creating components and stying, rdy for FaceRec functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from "react";
+import Particles from "react-particles-js";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import "./App.css";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 90,
+      dentsity: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className="particles" params={particlesOptions} />
         <Navigation />
         <Logo />
         <Rank />
@@ -25,6 +39,6 @@ Starting components:
   - Navigation      - COMPLETED
   - Logo            - COMPLETED
   - Rank            - COMPLETED
-  - ImageLinkForm   
+  - ImageLinkForm   - COMPLETED
   - FaceRecognition 
 */
